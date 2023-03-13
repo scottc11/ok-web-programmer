907b5fc861b7a5b32263a8dad684a96ab3720be8
Manuel Hinojosa <mangud.hinojosa@gmail.com>
2023-03-13 14:41:01 -0400

    Added 3 div containers to step one container Added title for step 1 Added input type text for step 1 Added instructions text for step 1

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -5,7 +5,22 @@ const Steps = () => {
   return (
     <div className="stepsContainer">
       <div className="stepContainer">
-        <div className="step"></div>
+        <div className="step">
+          <div className="instructionTitle">Enter Bootloader Mode</div>
+          <div className="bootloaderContainer">
+            <input type="text" list="options" />
+            <datalist id="options">
+              <option>STM BOOTLOADER</option>
+              <option>option two</option>
+              <option>option three</option>
+              <option>option four</option>
+            </datalist>
+          </div>
+          <div className="instructionText">
+            <sup>*</sup>Select STM BOOTLOADER from the list and then click
+            'Connect' button".
+          </div>
+        </div>
         <div className="checkBtn">
           <span className="checkmark"></span>
         </div>
