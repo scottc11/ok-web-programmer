c6edc0b06b7333208abb5968099a05c57919ad50
scottc11 <scott.m.campbell11@gmail.com>
2023-05-21 08:36:49 -0400

    setup connect, disconnect functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,93 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import dfu from "./dfu-util-js/dfu";
 import "./App.scss";
 import Header from "./components/Header/Header";
-import Instructions from "./components/Instructions/Instructions";
-import ProgressBar from "./components/ProgessBar/ProgressBar";
-import Steps from "./components/Steps/Steps";
-import Message from "./components/Message/Message";
 
 function App() {
-  const [areInstructionsDisplayed, setAreInstructionsDisplayed] =
-    useState(false);
 
-  const [isComplete, setIsComplete] = useState(true);
+  const [device, setDevice] = useState<USBDevice>(null);
+  const [file, setFile] = useState<any>(null);
+  const [deviceName, setDeviceName] = useState('');
+  const [deviceMFG, setDeviceMFG] = useState('');
+  const [deviceSerial, setDeviceSerial] = useState('');
 
-  const getDevices = () => {
-    // navigator.usb.getDevices().then( (devices: USBDevice[]) =>{
-    //   console.log(devices);
-    // })
-
+  const connect = () => {
     navigator.usb.requestDevice({ filters: [] }).then((device: USBDevice) => {
       let interfaces = dfu.findDeviceDfuInterfaces(device);
+      setDevice(device);
+      console.log(device);
+      const settings = interfaces[0];
+      console.log(settings);
+      const interfaceNumber = settings.interface.interfaceNumber;
+      console.log(interfaceNumber);
+
+      if (device) {
+        setDeviceName(device.productName);
+        setDeviceMFG(device.manufacturerName);
+        setDeviceSerial(device.serialNumber);
+        device.open();
+      }
+
     });
   };
+
+  const disconnect = () => {
+    if (device) {
+      device.close();
+      console.log(`Disconnected from ${device.productName}`)
+    }
+  }
+
+  const chooseFile = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      
+      console.log(event.target.files);
+      
+      const reader = new FileReader();
+      
+      reader.onload = () => {
+        setFile(reader.result);
+      };
+      
+      reader.readAsArrayBuffer(event.target.files[0]);
+    }
+  }
+
+  const upload = () => {
+    if (device && file) {
+      console.log('upload');
+    }
+  }
+
+  useEffect(() => {
+    console.log(file);
+  }, [file]);
+
 
   return (
     <div className="app">
       <Header />
-      <Instructions
-        areInstructionsDisplayed={areInstructionsDisplayed}
-        setAreInstructionsDisplayed={setAreInstructionsDisplayed}
-      />
-      <ProgressBar />
-      <Steps />
-
-      {isComplete ? <Message /> : ""}
+      <div>
+        <button onClick={() => connect()}>Connect</button>
+        
+        <button onClick={() => disconnect()}>Disconnect</button>
+        
+        <p>{deviceName}</p>
+        <p>{deviceMFG}</p>
+        <p>{deviceSerial}</p>
+        <div>
+          <input
+            type="file"
+            id="firmwareFile"
+            name="file"
+            disabled={false}
+            onChange={(e)=> chooseFile(e)}
+          />
+        </div>
+        <div>
+          <button disabled={device && file ? false : true} onClick={() => upload()}>Upload</button>
+        </div>
+      </div>
     </div>
   );
 }
