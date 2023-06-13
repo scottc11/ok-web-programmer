5fb45b9d17bf2d5fd2165dfb632928f6595fb470
scottc11 <scott.m.campbell11@gmail.com>
2023-06-13 08:43:56 -0400

    updated logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.scss";
-import logo from "../../media/logo.png";
+import logo from "../../media/OK200.png";
 
 const Header: React.FC = () => {
   return (
